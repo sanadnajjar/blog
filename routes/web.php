c41c63ed1d25a27b3117d36698de7f6ee008b663
Sanad <Sanadnajjar1996@gmail.com>
2019-07-25 11:58:31 +0300

    admin master page setup

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,13 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index.blade.php')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/admin', function () {
+
+    return view('admin.index');
+
+});
+
+Route::resource('admin/users', 'AdminUsersController');
+
